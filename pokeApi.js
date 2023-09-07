331f6a77df17a095e7502d1dea98a295a7d2c553
Victoria Martinez <vicotita1999@gmail.com>
2023-09-07 11:21:09 -0300

    Mejoras

--- pokeApi.js
+++ pokeApi.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     let contenedor = document.getElementById("contenedor");
+
     fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
+
         let numPoke = 0;
         let numID = 0;
+
         data.results.forEach(element => {
             let pokemon = element.name;
             fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
             .then((response) => response.json())
             .then((data) => {
 
-                console.log(data);
                 let pokeDiv = document.createElement("div");
                 let srcimage = data.sprites.front_default;
                 numPoke += 1;
@@ -23,10 +25,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
 
                 pokeDiv.innerHTML = `
-                <div style="width:182px; height: 310px; margin: 10px" class="card">
-                    <img class="card-img-top" src="${srcimage}" style="height:180px; width:180px; background-image: url('fondo.png');">
+                <div class="card">
+                    <img class="card-img-top" src="${srcimage}">
                     <div class="card-body position-relative">
-                        <h6 style="text-transform:uppercase;" class="card-title text-center">${pokemon} <span class="text-muted"> #${numPoke}</span></h6>
+                        <h6 class="card-title text-center">${pokemon} <span class="text-muted"> #${numPoke}</span></h6>
                         <div class="row" id="${numID}">
                         </div>
                     </div>
@@ -38,11 +40,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 data.types.forEach(element => {
                     divTipo.innerHTML += `
                     <div class="col">
-                        <p style="width:60px; background-color: rgb(240,214,111); font-size: 14px" class="border border-primary rounded-1 text-center position-absolute bottom-0">${element.type.name}</p>
+                        <p class="border rounded-1 text-center position-absolute bottom-0 ${element.type.name}">${element.type.name}</p>
                     </div>`;
                 });
 
             })
+
         });
+
     })
+
 })
